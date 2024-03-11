1bfefd07333a32788e1491641b399ee7f1197764
hectapia <oli21007@byui.edu>
2024-03-10 20:58:48 -0600

    Changing fields in database

--- controllers/users.js
+++ controllers/users.js
@@ -23,10 +23,11 @@ const getSingle = async (req, res) => {
 const createUser = async (req, res) => {
     //#swagger.tags=['Users']
     const user = {
-        email: req.body.email,
-        name: req.body.name,
-        username: req.body.username,
-        ipAddress: req.body.ipAddress
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName, 
+        email: req.body.email, 
+        favoriteColor: req.body.favoriteColor, 
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
     if (response.acknowledged) {
@@ -40,10 +41,11 @@ const updateUser = async (req, res) => {
     //#swagger.tags=['Users']
     const userId = new ObjectId(req.params.id);
     const user = {
-        email: req.body.email,
-        name: req.body.name,
-        username: req.body.username,
-        ipAddress: req.body.ipAddress
+        firstName: req.body.firstName, 
+        lastName: req.body.lastName, 
+        email: req.body.email, 
+        favoriteColor: req.body.favoriteColor, 
+        birthday: req.body.birthday
     };
     const response = await mongodb.getDatabase().db().collection('users').replaceOne({_id: userId},user);
     if (response.modifiedCount > 0) {
